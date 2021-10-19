1caa9c757775548c8feaacebcc3d38b8ad717de7
throzyrt <tahirozyurt78@gmail.com>
2021-10-19 19:25:32 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,29 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun=9.20
+let dolarBugun=9.30
+
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun=11.2
+//euroDun=11 const sabit değerlidir sonradan değiştirilemez
+
+console.log(euroDun)
+
+let konutKredileri=["Konut Kredileri","Emlak Konut Kredileri","Arsa Kredileri","Özel Konut Kredileri"]
+
+console.log(konutKredileri)
+
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+
+
+}
+console.log("/<ul>")
+
+
+
